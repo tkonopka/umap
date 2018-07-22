fe3773a7b9b1fd689366d79993256afacf5727cf
tkonopka <tokonopka@gmail.com>
2018-07-22 20:10:07 +0100

    test_python adjustments to avoid umap-learn warnings

--- tests/testthat/test_python.R
+++ tests/testthat/test_python.R
@@ -2,17 +2,7 @@
 
 cat("\ntest_python\n")
 library(reticulate)
-
-
-i.train = c(1:12, 61:72, 121:132)
-i.columns = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
-## one small dataset
-i4 = iris[i.train, i.columns]
-rownames(i4) = paste0("X", 1:nrow(i4))
-## another dataset for testing
-i.test = c(13:16, 73:76, 123:126)
-i4b = iris[i.test, i.columns]
-rownames(i4b) = paste0("Test", 1:nrow(i4b))
+source("train_test.R")
 
 
 
@@ -26,7 +16,8 @@ if (reticulate::py_module_available("umap")) {
   ## devtools::test() stops when skip() is invoked
 
   ## create embedding of small iris data
-  u1 = umap(i4, method="umap-learn", random_state=111)
+  ## must use large n_neighbors to avoid python warnings
+  u1 = umap(i.train, method="umap-learn", random_state=567, n_neighbors=42)
   
   
   test_that("python umap produces output", {
@@ -38,8 +29,8 @@ if (reticulate::py_module_available("umap")) {
     ## python implementation sets config, conveys arguments used
     expect_gt(length(u1$config$umap_learn_args), 5)
     ## output layout makes sense, has rownames
-    expect_equal(dim(u1$layout), c(nrow(i4), 2))
-    expect_equal(rownames(u1$layout), rownames(i4))
+    expect_equal(dim(u1$layout), c(nrow(i.train), 2))
+    expect_equal(rownames(u1$layout), rownames(i.train))
     ## python implementation returns a UMAP object
     expect_true("UMAP" %in% names(u1))
   })
@@ -47,9 +38,11 @@ if (reticulate::py_module_available("umap")) {
   
   test_that("python umap can use specified arguments", {
     conf = umap.defaults
+    conf$random_state = 567
+    conf$n_neighbors = 42
     conf$umap_learn_args = c("n_neighbors", "random_state")
-    result = umap(i4, conf, method="umap-learn")
-    expect_message(umap(i4, conf, method="umap-learn", verbose=1), "calling")
+    result = umap(i.train, conf, method="umap-learn")
+    expect_message(umap(i.train, conf, method="umap-learn", verbose=1), "calling")
     ## python implementation sets config, conveys arguments used
     expect_equal(length(result$config$umap_learn_args), 2)
   })
@@ -57,18 +50,17 @@ if (reticulate::py_module_available("umap")) {
   
   test_that("python umap considers user-specified inputs", {
     uconf = umap.defaults
-    uconf$n_epochs = 5
-    uconf$n_neighbors = 5
-    uconf$random_state = 123
+    uconf$n_neighbors = 42
+    uconf$random_state = 567
     ## repeat calculations should give same results
-    result1 = umap(i4, uconf, method="umap-learn")
-    result2 = umap(i4, uconf, method="umap-learn")
+    result1 = umap(i.train, uconf, method="umap-learn")
+    result2 = umap(i.train, uconf, method="umap-learn")
     ## check reproducible results in layout
     expect_true(identical(result1$layout, result2$layout))
     ## calculations with different setting should give different result
-    result3 = umap(i4, uconf, method="umap-learn", n_neighbors=6)
+    result3 = umap(i.train, uconf, method="umap-learn")
     expect_false(identical(result1, result3))
-    result4 = umap(i4, uconf, method="umap-learn", spread=2)
+    result4 = umap(i.train, uconf, method="umap-learn", spread=2)
     expect_false(identical(result3, result4))
   })
 
@@ -76,17 +68,17 @@ if (reticulate::py_module_available("umap")) {
   test_that("transform requires presence of UMAP components", {
     u1small = u1
     u1small$UMAP = NULL
-    expect_error(predict(u1small, i4b), "available")
+    expect_error(predict(u1small, i.test), "available")
     u1bad = u1
     u1bad$UMAP = "some other object"
-    expect_error(predict(u1bad, i4b), "corrupt")
+    expect_error(predict(u1bad, i.test), "corrupt")
   })
 
   
-  test_that("transform proeduces and embedding", {
-    u2 = predict(u1, i4b)
-    expect_equal(dim(u2), c(nrow(i4b), 2))
-    expect_equal(rownames(u2), rownames(i4b))
+  test_that("transform proeduces an embedding", {
+    u2 = predict(u1, i.test)
+    expect_equal(dim(u2), c(nrow(i.test), 2))
+    expect_equal(rownames(u2), rownames(i.test))
   })
   
 }
